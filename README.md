6db5338287b2709f87efab1265ceb9ed629d2644
Niklas Mollenhauer <nikeee@outlook.com>
2020-03-18 06:24:18 +0100

    Hi!

---
+++ README.md
@@ -0,0 +1,50 @@
+# dotnet-alphasecret [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://raw.githubusercontent.com/nikeee/dotnet-alphasecret/master/LICENSE) ![Build](https://github.com/nikeee/dotnet-alphasecret/workflows/Build/badge.svg)
+> .NET Core implementation of [Hanno Böck's Alphasecret](https://github.com/hannob/alphasecret).
+
+Excerpt from hannob's readme:
+> GIMP has an unexpected behavior that when "deleting" content from an image with an alpha channel it will not actually delete the content, it will just be marked transparent.
+>
+> This can have the unintended sideeffect of leaking information if you use GIMP to remove private parts of an image, e.g. a screenshot.
+
+## Download
+Check out the [releases section](https://github.com/nikeee/dotnet-alphasecret/releases) for the latest builds.
+
+## Usage
+```
+# Inspect folder:
+./AlphaSecret examples
+
+# Inspect single file
+./AlphaSecret examples/color-suspicious.png
+
+# Reading targets from stdin:
+ls -1 **/*.png | ./AlphaSecret --stdin
+```
+
+## Building from Source
+```
+git clone https://github.com/nikeee/dotnet-alphasecret
+
+# Linux:
+dotnet publish -c Release --self-contained --runtime linux-x64
+# Resulting executable will be in:
+./bin/Release/netcoreapp3.1/linux-x64/publish/AlphaSecret
+
+# Windows:
+dotnet publish -c Release --self-contained --runtime win-x64
+# Resulting executable will be in:
+./bin/Release/netcoreapp3.1/win-x64/publish/AlphaSecret.exe
+
+# Linux:
+dotnet publish -c Release --self-contained --runtime osx-x64
+# Resulting executable will be in:
+./bin/Release/netcoreapp3.1/osx-x64/publish/AlphaSecret
+```
+
+## Why?
+- Calling ImageMagick's `convert` and piping `hexdump` into `grep` works for a few images, but will be slow on millions of images.
+- A bash script does not run very well on windows (WSL might help, but it has poor IO performance)
+
+## Background
+- News article: https://www.golem.de/news/alphakanal-gimp-verraet-geheimnisse-in-bildern-2002-146504.html
+- Discusson on Gitlab: https://gitlab.gnome.org/GNOME/gimp/issues/4487
---
+++ src/Program.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Linq;
+using System.IO;
+using CommandLine;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Collections.Generic;
+
+namespace AlphaSecret
+{
+    class Program
+    {
+
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+            [Option("overwrite", Required = false, HelpText = "Overwrite exisiting files when restoring files.")]
+            public bool Overwrite { get; set; }
+            [Option('n', "no-restore", Required = false, HelpText = "Disabled restoring of suspicious files.")]
+            public bool NoRestore { get; set; }
+            [Option("stdin", Required = false, HelpText = "Read paths to process form stdin.")]
+            public bool StdIn { get; set; }
+
+            [Value(0)]
+            public IEnumerable<string>? InputDirectoriesAndFiles { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            {
+                if (o.StdIn)
+                {
+                    if (o.Verbose)
+                        Console.WriteLine("Reading paths from stdin");
+
+                    var line = Console.ReadLine();
+                    while (line != null)
+                    {
+                        ProcessPath(o, line);
+                        line = Console.ReadLine();
+                    }
+                }
+                else if (o.InputDirectoriesAndFiles != null)
+                {
+                    foreach (var inputDirOrFile in o.InputDirectoriesAndFiles)
+                        ProcessPath(o, inputDirOrFile);
+                }
+                else
+                {
+                    Console.WriteLine("No files to process passed.");
+                }
+            });
+        }
+
+
+        static void ProcessPath(Options options, string path)
+        {
+            try
+            {
+                var attrs = File.GetAttributes(path);
+                if (attrs.HasFlag(FileAttributes.Directory))
+                    ProcessDirectory(options, path);
+                else
+                    ProcessFile(options, path);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error processing file or directory \"{path}\": {ex.Message}");
+            }
+        }
+
+        static void ProcessDirectory(Options options, string directoryPath)
+        {
+            var candidatePaths = Directory.EnumerateFiles(directoryPath, "*.png", new EnumerationOptions()
+            {
+                MatchCasing = MatchCasing.CaseInsensitive,
+                MatchType = MatchType.Simple,
+                RecurseSubdirectories = true,
+            });
+
+            if (options.Verbose)
+                Console.WriteLine($"Enumerating files in directory: {directoryPath}");
+
+            candidatePaths.AsParallel().ForAll(file => ProcessFile(options, file));
+        }
+
+        static void ProcessFile(Options options, string path)
+        {
+            using var img = Image.Load<Rgba32>(path);
+
+            var isImageSuspicious = IsImageSuspicious(img);
+
+            if (isImageSuspicious)
+            {
+                Console.WriteLine(
+                    options.Verbose
+                        ? $"Image is suspicious: {path}"
+                        : path
+                );
+
+                if (!options.NoRestore)
+                {
+                    RestoreImageData(img);
+
+                    var restoredPath = Path.ChangeExtension(path, Path.GetExtension(path) + "-restored");
+
+                    if (!options.Overwrite && File.Exists(restoredPath))
+                        Console.Error.WriteLine($"Could not restore image to {restoredPath}, file already exists");
+
+                    using var s = File.Create(restoredPath);
+                    img.SaveAsPng(s);
+                }
+
+            }
+            else if (options.Verbose)
+                Console.WriteLine($"File seems ok: {path}");
+        }
+
+        static int RestoreImageData(Image<Rgba32> img)
+        {
+            int restoredPixels = 0;
+            for (int x = 0; x < img.Width; ++x)
+            {
+                for (int y = 0; y < img.Height; ++y)
+                {
+                    var pixel = img[x, y];
+                    if (pixel.A == 0 && HasRgbData(pixel))
+                    {
+                        pixel.A = 255;
+                        img[x, y] = pixel;
+                        ++restoredPixels;
+                    }
+                }
+            }
+            return restoredPixels;
+        }
+
+        static bool IsImageSuspicious(Image<Rgba32> img)
+        {
+            for (int x = 0; x < img.Width; ++x)
+            {
+                for (int y = 0; y < img.Height; ++y)
+                {
+                    var pixel = img[x, y];
+                    if (pixel.A == 0 && HasRgbData(pixel))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        static bool HasRgbData(Rgba32 pixel) => (pixel.R | pixel.G | pixel.G) != 0;
+    }
+}
